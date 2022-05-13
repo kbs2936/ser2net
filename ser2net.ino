1d9c6450403031437e908f0c9d4929732fc8cfdb
刘煌 <225225@nd.com>
2022-05-13 21:20:52 +0800

    second blood

--- ser2net.ino
+++ ser2net.ino
@@ -1,7 +1,8 @@
 #include "config.h"
-#include <WiFiManager.h> //https://github.com/tzapu/WiFiManager
+#include <WiFiManager.h>
+#include <WiFiClient.h>
 
-// For debug log output/update FW
+// For debug log output
 #if (DEBUG)
 HardwareSerial *DBGCOM = &Serial1;
 #define LOGD(...)                \
@@ -16,8 +17,23 @@ HardwareSerial *DBGCOM = &Serial1;
 
 HardwareSerial *COM = &Serial;
 
+WiFiServer *server;
+WiFiClient *TCPClient[MAX_NMEA_CLIENTS];
+uint8_t buf1[BUFFERSIZE];
+uint16_t i1 = 0;
+uint8_t buf2[BUFFERSIZE];
+uint16_t i2 = 0;
+
+static inline void reset_zigbee()
+{
+  digitalWrite(ZGB_RST_PIN, LOW);
+  delay(500);
+  digitalWrite(ZGB_RST_PIN, HIGH);
+}
+
 void setup()
 {
+  // opt3：USB TTY <==> ESP 8266 debug port UART1.  ESP 8266 TTY UART0 <==> E18 TTY
   pinMode(TTY_SEL0_PIN, OUTPUT);
   digitalWrite(TTY_SEL0_PIN, LOW);
   pinMode(TTY_SEL1_PIN, OUTPUT);
@@ -31,7 +47,7 @@ void setup()
 #endif
 
   /*
-  配置 WIFI
+  配置 WIFI，在 loop 中断网，网络恢复后会自动重连上，不需要用 if (WiFi.status() == WL_CONNECTED) 去判断网络和做重连的操作
   上次如果有配置过wifi账号密码，启动后autoConnect如果连接不成功会等待20s，10s内把路由那边的wifi密码开起来还能连上不会自动变AP，
   不设这个超时连接失败是立刻变AP
   */
@@ -45,24 +61,112 @@ void setup()
   //网络设置界面exit会走返回这个false。web界面wifi密码连接错误此方法不会返回，还是重新变回ap
   if (!wifiManager.autoConnect(apName.c_str()))
   {
-    LOGD("failed to connect, now reset");
+    LOGD("Failed to connect, now reset");
     delay(100);
     ESP.reset();
   }
 
-  LOGD("connect wifi success");
+  // start TCP server
+  LOGD("TCP server: %s:%d", WiFi.localIP().toString().c_str(), TCP_PORT);
+  static WiFiServer server_0(TCP_PORT);
+  server = &server_0;
+  server->begin();
+  server->setNoDelay(true);
+
+  pinMode(ZGB_RST_PIN, OUTPUT);
+  digitalWrite(ZGB_RST_PIN, HIGH);
+  reset_zigbee();
 }
 
 void loop()
 {
-  if (WiFi.status() == WL_CONNECTED)
+  for (byte i = 0; i < MAX_NMEA_CLIENTS; i++)
   {
-    LOGD("YES");
-  }
-  else
-  {
-    LOGD("NO");
+    //find disconnected spot
+    if (TCPClient[i] && !TCPClient[i]->connected())
+    {
+      TCPClient[i]->stop();
+      delete TCPClient[i];
+      TCPClient[i] = NULL;
+      LOGD("Client disconnected");
+    }
   }
 
-  delay(2000);
+  if (server->hasClient())
+  {
+    for (byte i = 0; i < MAX_NMEA_CLIENTS; i++)
+    {
+      // find free/disconnected spot
+      if (!TCPClient[i] || !TCPClient[i]->connected())
+      {
+        if (TCPClient[i])
+        {
+          TCPClient[i]->stop();
+          delete TCPClient[i];
+          TCPClient[i] = NULL;
+          LOGD("Client disconnected");
+        }
+        TCPClient[i] = new WiFiClient;
+        *TCPClient[i] = server->available();
+        LOGD("New client for COM");
+      }
+    }
+    // no free/disconnected spot so reject
+    // WiFiClient TmpserverClient = server->available();
+    // TmpserverClient.stop();
+  }
+
+  if (COM != NULL)
+  {
+    for (byte cln = 0; cln < MAX_NMEA_CLIENTS; cln++)
+    {
+      if (TCPClient[cln])
+      {
+        while (TCPClient[cln]->available())
+        {
+          buf1[i1] = TCPClient[cln]->read(); // read char from TCP port:8880
+          if (i1 < BUFFERSIZE - 1)
+            i1++;
+        }
+        if (i1 > 0)
+        {
+          COM->write(buf1, i1); // now send to UART
+#if (DEBUG)
+          DBGCOM->printf("TX(%d):\t", i1);
+          for (int i = 0; i < i1; i++)
+            DBGCOM->printf("%02x ", buf1[i]);
+          DBGCOM->println();
+#endif
+          i1 = 0;
+        }
+      }
+    }
+
+    if (COM->available())
+    {
+      while (COM->available())
+      {
+        buf2[i2] = COM->read(); // read char from UART
+        if (i2 < BUFFERSIZE - 1)
+          i2++;
+      }
+
+      if (i2 > 0)
+      {
+        // now send to WiFi:
+        for (byte cln = 0; cln < MAX_NMEA_CLIENTS; cln++)
+        {
+          if (TCPClient[cln])
+            TCPClient[cln]->write(buf2, i2); // send the buffer to TCP port:8880
+        }
+#if (DEBUG)
+        DBGCOM->printf("RX(%d):\t", i2);
+        for (int i = 0; i < i2; i++)
+          DBGCOM->printf("%02x ", buf2[i]);
+        DBGCOM->println();
+#endif
+        i2 = 0;
+      }
+    }
+  }
 }
