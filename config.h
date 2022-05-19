68d5f3b4172a63e48207ea016d566d9257a422f5
刘煌 <225225@nd.com>
2022-05-19 16:35:51 +0800

    优化代码结构

--- config.h
+++ config.h
@@ -1,26 +1,35 @@
 #ifndef _ESP_SERIAL_BRIDGE_CONFIG_H_
 #define _ESP_SERIAL_BRIDGE_CONFIG_H_
 
-//最大tcp client连接数
+//最大tcp client连接数和默认TCP服务器端口
 #define MAX_NMEA_CLIENTS 1
-#define TCP_PORT         8880
+#define TCP_PORT 8880
 
-//拨盘开关连接8266芯片的引脚号定义
-#define TTY_SEL0_PIN    16
-#define TTY_SEL1_PIN    5
+//拨盘开关和zigbee复位引脚
+#define TTY_SEL0_PIN 16
+#define TTY_SEL1_PIN 5
+#define ZGB_RST_PIN 12
 
-#define ZGB_RST_PIN     12
-
-//ws2812b 灯的引脚和个数定义
+// ws2812b灯的引脚和个数
 #define LED_PIN 15
 #define NUM_LEDS 1
 
-/*************************  COM Port 0 *******************************/
-#define UART_BAUD0 115200        // Baudrate UART0
-#define SERIAL_PARAM0 SERIAL_8N1 // Data/Parity/Stop UART0
+//通讯串口0波特率和模式
+#define UART_BAUD0 115200
+#define SERIAL_PARAM0 SERIAL_8N1
 
 #define BUFFERSIZE 1024
 
 #define DEBUG 1
+
+//灯颜色枚举
+enum LedColor
+{
+    LedColorRed = 0,
+    LedColorGreen = 1,
+    LedColorBlue = 2,
+    LedColorBlack = 3,
+    LedColorOff = 4
+};
 
 #endif
--- ser2net.ino
+++ ser2net.ino
@@ -3,7 +3,8 @@
 #include <WiFiClient.h>
 #include <Adafruit_NeoPixel.h>
 
-//串口1，打印调试日志
+//---------------------------------------------------------------------------------------------------------------------
+// Debug串口1，打印调试日志
 #if (DEBUG)
 HardwareSerial *DBGCOM = &Serial1;
 #define LOGD(...)                \
@@ -16,7 +17,7 @@ HardwareSerial *DBGCOM = &Serial1;
 #define LOGD(...)
 #endif
 
-//串口0，对接2530串口
+//通讯串口0，对接2530串口
 HardwareSerial *COM = &Serial;
 
 // wifi server和数据收发缓冲区
@@ -27,24 +28,17 @@ uint16_t i1 = 0;
 uint8_t buf2[BUFFERSIZE];
 uint16_t i2 = 0;
 
-//loop过程中检测到wifi断开连接，则复位此标志位
+// loop过程中检测到wifi断开连接，则复位此标志位
 bool isWiFiConnected = false;
-
-//灯颜色枚举
-enum LedColor
-{
-  LedColorRed = 0,
-  LedColorGreen = 1,
-  LedColorBlue = 2,
-  LedColorBlack = 3,
-  LedColorOff = 4
-};
 
 //(灯总数,使用引脚,WS2812B一般都是800这个参数不用动)
 Adafruit_NeoPixel WS2812B(1, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-
-//配置灯的颜色函数
+//---------------------------------------------------------------------------------------------------------------------
+/**
+ * @description: 配置灯的颜色
+ * @param color：LedColor
+ */
 void ledShowColor(LedColor color)
 {
   switch (color)
@@ -71,22 +65,26 @@ void ledShowColor(LedColor color)
     break;
 
   default:
-
     break;
   }
 }
 
-/********************
-   设置灯带中某一个灯的颜色。
-   单灯颜色(灯位置,红,绿,蓝)
-   下标从0开始
-********************/
+/**
+ * @description: 设置灯带中某一个灯的颜色
+ * @param index：灯的下标，从0开始
+ * @param R：红 0~255
+ * @param G：绿 0~255
+ * @param B：蓝 0~255
+ */
 void singleLedColor(int index, int R, int G, int B)
 {
   WS2812B.setPixelColor(index, (((R & 0xffffff) << 16) | ((G & 0xffffff) << 8) | B));
   WS2812B.show();
 }
 
+/**
+ * @description: 复位E18模块
+ */
 void resetZigbee()
 {
   digitalWrite(ZGB_RST_PIN, LOW);
@@ -95,72 +93,11 @@ void resetZigbee()
   LOGD("Reset zigbee");
 }
 
-// setup
-void setup()
+/**
+ * @description：检查网络状态，断网亮红灯，重新联网亮绿灯
+ */
+void checkNetwork()
 {
-  //8266复位也同时复位一下zigbee
-  pinMode(ZGB_RST_PIN, OUTPUT);
-  digitalWrite(ZGB_RST_PIN, HIGH);
-  delay(100);
-  resetZigbee();
-
-  //配置LED灯，未联网红色、联网绿色、node连进来蓝色
-  WS2812B.begin();
-  WS2812B.clear();
-  WS2812B.setBrightness(80);
-  ledShowColor(LedColorRed);
-
-  /*
-  拨盘开关引脚配置
-  opt3：USB TTY <==> ESP 8266 debug port UART1.  ESP 8266 TTY UART0 <==> E18 TTY
-  */
-  pinMode(TTY_SEL0_PIN, OUTPUT);
-  digitalWrite(TTY_SEL0_PIN, LOW);
-  pinMode(TTY_SEL1_PIN, OUTPUT);
-  digitalWrite(TTY_SEL1_PIN, HIGH);
-  delay(500);
-
-  COM->begin(UART_BAUD0, SERIAL_PARAM0);
-
-#if (DEBUG)
-  DBGCOM->begin(115200);
-#endif
-
-  /*
-  配置 WIFI，在 loop 中断网，网络恢复后会自动重连上，不需要用 if (WiFi.status() == WL_CONNECTED) 去判断网络和做重连的操作
-  上次如果有配置过wifi账号密码，启动后autoConnect如果连接不成功会等待20s，10s内把路由那边的wifi密码开起来还能连上不会自动变AP，
-  不设这个超时连接失败是立刻变AP
-  */
-  WiFiManager wifiManager;
-  wifiManager.setConnectTimeout(10);
-  // randomSeed(analogRead(15));
-  // String apName = String("ZigBeeAP_") + String(random(100, 999)); //先不用随机数了，使用芯片id做ap和host的名字
-  String apName = String("ZigBeeGateWay_") + String(ESP.getChipId());
-  wifiManager.setHostname(apName);
-  LOGD("\n\n Begin connect wifi, apName = %s", apName.c_str());
-
-  //网络设置界面exit会走返回这个false。web界面wifi密码连接错误此方法不会返回，还是重新变回ap
-  if (!wifiManager.autoConnect(apName.c_str()))
-  {
-    LOGD("[ERROR] Failed to connect, now reset");
-    delay(100);
-    ESP.reset();
-  }
-
-  // start TCP server
-  isWiFiConnected = true;
-  ledShowColor(LedColorGreen);
-  LOGD("TCP server: %s:%d", WiFi.localIP().toString().c_str(), TCP_PORT);
-  static WiFiServer server_0(TCP_PORT);
-  server = &server_0;
-  server->begin();
-  server->setNoDelay(true);
-}
-
-// loop
-void loop()
-{
-  //断网亮灯，重新联网灭灯
   if (WiFi.status() != WL_CONNECTED)
   {
     //通过标志位，loop期间断网不要重复设置灯红色，只要设置一次即可，网络恢复同理也不要灭灯多次
@@ -170,8 +107,6 @@ void loop()
       ledShowColor(LedColorRed);
       LOGD("[ERROR] Wifi disconnect");
     }
-    delay(200);
-    return;
   }
   else
   {
@@ -182,8 +117,14 @@ void loop()
     }
     isWiFiConnected = true;
   }
+}
 
-  //把所有断开的客户端移除
+/**
+ * @description：检查 tcp client(zigbee2mqtt node)的连接状态，已断开的移除，新连接的加到数组 
+ */
+void checkTcpClient()
+{
+  //先把所有断开的客户端移除
   for (byte i = 0; i < MAX_NMEA_CLIENTS; i++)
   {
     if (TCPClient[i] && !TCPClient[i]->connected())
@@ -193,7 +134,7 @@ void loop()
       TCPClient[i] = NULL;
       LOGD("[ERROR] Client disconnected 1");
       ledShowColor(LedColorGreen);
-      resetZigbee(); //node断开时复位一下 zigbee，避免node再连上时候无法握手通信
+      resetZigbee(); // node断开时复位一下 zigbee，避免node再连上时候无法握手通信
     }
   }
 
@@ -213,23 +154,23 @@ void loop()
           TCPClient[i] = NULL;
           LOGD("[ERROR] Client disconnected 2");
           ledShowColor(LedColorGreen);
-          resetZigbee(); //node断开时复位一下 zigbee，避免node再连上时候无法握手通信
+          resetZigbee(); // node断开时复位一下 zigbee，避免node再连上时候无法握手通信
         }
         /*
         堆上开辟对象内存，指针指向它，然后把新进客户端的成员属性赋值给它，对象内存还是2个，只是成员变量值相同
         为什么用指针？直接2个对象赋值不就好了
         */
-        TCPClient[i] = new WiFiClient; 
+        TCPClient[i] = new WiFiClient;
         *TCPClient[i] = server->available();
         LOGD("New client for COM");
         ledShowColor(LedColorBlue);
       }
     }
     //遍历之后，如果之前数组里的客户端已满，并且都是连接的状态，无法接受新的客户端，则把新客户端读走并 stop
-    WiFiClient TmpserverClient = server->available();
-    if (TmpserverClient)
+    WiFiClient tmpServerClient = server->available();
+    if (tmpServerClient)
     {
-      TmpserverClient.stop();
+      tmpServerClient.stop();
       LOGD("[ERROR] Client's array is full, stop new coming");
     }
     else
@@ -237,60 +178,150 @@ void loop()
       LOGD("All clients are accepted");
     }
   }
+}
 
-  if (COM != NULL)
+/**
+ * @description: 读取 tcp client 发来的数据，然后通过 串口0-tx 发给E18
+ */
+void wifi2Zigbee()
+{
+  for (byte cln = 0; cln < MAX_NMEA_CLIENTS; cln++)
   {
-    //读取 tcp client 发来的数据，然后通过 串口0 tx 发给2530
-    for (byte cln = 0; cln < MAX_NMEA_CLIENTS; cln++)
+    if (TCPClient[cln])
     {
-      if (TCPClient[cln])
+      while (TCPClient[cln]->available())
       {
-        while (TCPClient[cln]->available())
-        {
-          buf1[i1] = TCPClient[cln]->read(); // read char from TCP port:8880
-          if (i1 < BUFFERSIZE - 1)
-            i1++;
-        }
-        if (i1 > 0)
-        {
-          COM->write(buf1, i1); // now send to UART
-#if (DEBUG)
-          DBGCOM->printf("W->Z(%d):\t", i1);
-          for (int i = 0; i < i1; i++)
-            DBGCOM->printf("%02x ", buf1[i]);
-          DBGCOM->println();
-#endif
-          i1 = 0;
-        }
+        buf1[i1] = TCPClient[cln]->read(); // read char from TCP port:8880
+        if (i1 < BUFFERSIZE - 1)
+          i1++;
       }
-    }
-
-    //读取2530发来的数据，发给 tcp client
-    if (COM->available())
-    {
-      while (COM->available())
+      if (i1 > 0)
       {
-        buf2[i2] = COM->read(); // read char from UART
-        if (i2 < BUFFERSIZE - 1)
-          i2++;
-      }
-
-      if (i2 > 0)
-      {
-        // now send to WiFi:
-        for (byte cln = 0; cln < MAX_NMEA_CLIENTS; cln++)
-        {
-          if (TCPClient[cln])
-            TCPClient[cln]->write(buf2, i2); // send the buffer to TCP port:8880
-        }
+        COM->write(buf1, i1); // now send to UART
 #if (DEBUG)
-        DBGCOM->printf("Z->W(%d):\t", i2);
-        for (int i = 0; i < i2; i++)
-          DBGCOM->printf("%02x ", buf2[i]);
+        DBGCOM->printf("W->Z(%d):\t", i1);
+        for (int i = 0; i < i1; i++)
+          DBGCOM->printf("%02x ", buf1[i]);
         DBGCOM->println();
 #endif
-        i2 = 0;
+        i1 = 0;
       }
     }
   }
+}
+
+/**
+ * @description: 读取E18发来的数据(UART0-RX)，发给 tcp client
+ */
+void zigbee2Wifi()
+{
+  if (COM->available())
+  {
+    while (COM->available())
+    {
+      buf2[i2] = COM->read(); // read char from UART
+      if (i2 < BUFFERSIZE - 1)
+        i2++;
+    }
+
+    if (i2 > 0)
+    {
+      // now send to WiFi:
+      for (byte cln = 0; cln < MAX_NMEA_CLIENTS; cln++)
+      {
+        if (TCPClient[cln])
+          TCPClient[cln]->write(buf2, i2); // send the buffer to TCP port:8880
+      }
+#if (DEBUG)
+      DBGCOM->printf("Z->W(%d):\t", i2);
+      for (int i = 0; i < i2; i++)
+        DBGCOM->printf("%02x ", buf2[i]);
+      DBGCOM->println();
+#endif
+      i2 = 0;
+    }
+  }
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+/**
+ * @description: setup
+ */
+void setup()
+{
+  // 8266复位也同时复位一下zigbee
+  pinMode(ZGB_RST_PIN, OUTPUT);
+  digitalWrite(ZGB_RST_PIN, HIGH);
+  delay(100);
+  resetZigbee();
+
+  //配置LED灯，未联网红色、联网绿色、node连进来蓝色
+  WS2812B.begin();
+  WS2812B.clear();
+  WS2812B.setBrightness(80);
+  ledShowColor(LedColorRed);
+
+  /*
+  拨盘开关引脚配置
+  opt3：USB TTY <==> ESP 8266 debug port UART1.  ESP 8266 TTY UART0 <==> E18 TTY
+  */
+  pinMode(TTY_SEL0_PIN, OUTPUT);
+  digitalWrite(TTY_SEL0_PIN, LOW);
+  pinMode(TTY_SEL1_PIN, OUTPUT);
+  digitalWrite(TTY_SEL1_PIN, HIGH);
+  delay(100);
+
+  //通讯串口0和调试串口1初始化
+  COM->begin(UART_BAUD0, SERIAL_PARAM0);
+#if (DEBUG)
+  DBGCOM->begin(115200);
+#endif
+
+  /*
+  配置 WIFI，在 loop 中断网，网络恢复后会自动重连上，不需要用 if (WiFi.status() == WL_CONNECTED) 去判断网络和做重连的操作
+  上次如果有配置过wifi账号密码，启动后autoConnect如果连接不成功会等待10s，10s内把路由那边的wifi密码开起来还能连上不会自动变AP，
+  不设这个超时连接失败是立刻变AP
+  */
+  WiFiManager wifiManager;
+  wifiManager.setConnectTimeout(10);
+  // randomSeed(analogRead(15));
+  // String apName = String("ZigBeeAP_") + String(random(100, 999)); //先不用随机数了，使用芯片id做ap和host的名字
+  String apName = String("ZigBeeGateWay_") + String(ESP.getChipId());
+  wifiManager.setHostname(apName);
+  LOGD("\n\n Begin connect wifi, apName = %s", apName.c_str());
+
+  //网络设置界面exit会走返回这个false。web界面wifi密码连接错误此方法不会返回，还是重新变回ap
+  if (!wifiManager.autoConnect(apName.c_str()))
+  {
+    LOGD("[ERROR] Failed to connect, now reset");
+    delay(100);
+    ESP.reset();
+  }
+
+  //开启 TCP server
+  isWiFiConnected = true;
+  ledShowColor(LedColorGreen);
+  LOGD("TCP server: %s:%d", WiFi.localIP().toString().c_str(), TCP_PORT);
+  static WiFiServer server_0(TCP_PORT);
+  server = &server_0;
+  server->begin();
+  server->setNoDelay(true);
+}
+
+/**
+ * @description: loop
+ */
+void loop()
+{
+  //检查网络状态
+  checkNetwork();
+
+  //检查 tcp client(zigbee2mqtt node)的连接状态，已断开的移除，新连接的加到数组 
+  checkTcpClient();
+
+  //读取 tcp client 发来的数据，然后通过 串口0-tx 发给E18
+  wifi2Zigbee();
+
+  //读取E18发来的数据(UART0-RX)，发给 tcp client
+  zigbee2Wifi();
 }
